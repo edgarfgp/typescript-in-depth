b79ae77f5cea59159f935e27016546613526a023
Edgar Fabian Gonzalez Peña <edgargonzalez.developer@gmail.com>
2018-03-31 23:25:53 +0100

    basic variables and funtions has been added

--- app.ts
+++ app.ts
@@ -1,8 +1,78 @@
-class HelloWorld {
-
-    constructor(public name: string) {
-    }
+enum Category {
+    Biography,
+    Poetry,
+    Fiction,
+    History,
+    Children
 }
 
-let hello = new HelloWorld('Edgar');
-console.log(hello.name);
+function GetAllBooks() {
+    let books = [
+        { id: 1, title: 'Ulisses', author: 'James Royce', available: true, category: Category.Biography },
+        { id: 2, title: 'A Farewell to Arms', author: 'Ernest Hemingway', available: false, category: Category.Biography },
+        { id: 3, title: 'I Know the Caged Bird Sings ', author: 'Maya Angelou', available: true, category: Category.Biography },
+        { id: 4, title: 'I Mobby Dick ', author: 'herman Melville', available: true, category: Category.Fiction }
+    ];
+    return books;
+}
+
+function LogFirstAvailable(books: any[]): void {
+
+    let munberOfBooks: number = books.length;
+    let firstAvailable: string = '';
+
+    for (const currentBook of books) {
+        if (currentBook.available) {
+            firstAvailable = currentBook.title;
+            break;
+        }
+    }
+    console.log(`Total Books: ${munberOfBooks}`);
+    console.log(`Fisrt Available: ${firstAvailable}`);
+}
+
+function GetBookTitlesByCategory(categoryFilter: Category): Array<string> {
+    console.log(`Getting Books in category: ${Category[categoryFilter]}`);
+
+    const allBooks = GetAllBooks();
+    const filteredTitles: string[] = [];
+
+    allBooks.forEach(element => {
+        if (element.category === categoryFilter) {
+            filteredTitles.push(element.title);
+        }
+    });
+
+    return filteredTitles;
+}
+
+function LogBookTitles(titles: string[]): void {
+    titles.forEach(element => {
+        console.log(element);
+    });
+
+}
+
+function getBookById(id : number){
+    const allBooks = GetAllBooks();
+    return allBooks.filter(book => book.id === id)[0];
+}
+
+function CreateCustomerId(name: string, id: number): string{
+    return  `${name} --- ${id}`;
+}
+
+/***************************************************************************************** */
+// const ficctionBooks = GetAllBooks();
+// ficctionBooks.forEach((val, idx, arr) => console.log(`${++idx} - ${val.title}`));
+
+
+let Idgenerator: (chars: string, nums: number) => string;
+Idgenerator = CreateCustomerId;
+
+let myId = Idgenerator('Oscar', 10);
+
+console.log(myId)
+
+
+
--- js/app.js
+++ js/app.js
@@ -1,9 +1,61 @@
-var HelloWorld = /** @class */ (function () {
-    function HelloWorld(name) {
-        this.name = name;
+var Category;
+(function (Category) {
+    Category[Category["Biography"] = 0] = "Biography";
+    Category[Category["Poetry"] = 1] = "Poetry";
+    Category[Category["Fiction"] = 2] = "Fiction";
+    Category[Category["History"] = 3] = "History";
+    Category[Category["Children"] = 4] = "Children";
+})(Category || (Category = {}));
+function GetAllBooks() {
+    var books = [
+        { id: 1, title: 'Ulisses', author: 'James Royce', available: true, category: Category.Biography },
+        { id: 2, title: 'A Farewell to Arms', author: 'Ernest Hemingway', available: false, category: Category.Biography },
+        { id: 3, title: 'I Know the Caged Bird Sings ', author: 'Maya Angelou', available: true, category: Category.Biography },
+        { id: 4, title: 'I Mobby Dick ', author: 'herman Melville', available: true, category: Category.Fiction }
+    ];
+    return books;
+}
+function LogFirstAvailable(books) {
+    var munberOfBooks = books.length;
+    var firstAvailable = '';
+    for (var _i = 0, books_1 = books; _i < books_1.length; _i++) {
+        var currentBook = books_1[_i];
+        if (currentBook.available) {
+            firstAvailable = currentBook.title;
+            break;
+        }
     }
-    return HelloWorld;
-}());
-var hello = new HelloWorld('Edgar');
-console.log(hello.name);
+    console.log("Total Books: " + munberOfBooks);
+    console.log("Fisrt Available: " + firstAvailable);
+}
+function GetBookTitlesByCategory(categoryFilter) {
+    console.log("Getting Books in category: " + Category[categoryFilter]);
+    var allBooks = GetAllBooks();
+    var filteredTitles = [];
+    allBooks.forEach(function (element) {
+        if (element.category === categoryFilter) {
+            filteredTitles.push(element.title);
+        }
+    });
+    return filteredTitles;
+}
+function LogBookTitles(titles) {
+    titles.forEach(function (element) {
+        console.log(element);
+    });
+}
+function getBookById(id) {
+    var allBooks = GetAllBooks();
+    return allBooks.filter(function (book) { return book.id === id; })[0];
+}
+function CreateCustomerId(name, id) {
+    return name + " --- " + id;
+}
+/***************************************************************************************** */
+// const ficctionBooks = GetAllBooks();
+// ficctionBooks.forEach((val, idx, arr) => console.log(`${++idx} - ${val.title}`));
+var Idgenerator;
+Idgenerator = CreateCustomerId;
+var myId = Idgenerator('Oscar', 10);
+console.log(myId);
 //# sourceMappingURL=app.js.map
